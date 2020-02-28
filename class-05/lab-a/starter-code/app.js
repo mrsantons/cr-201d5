ab3c374611f96d81d879c8fbf68e767e732ac92a
Marie Antons <maantons@mail.fhsu.edu>
2020-02-28 14:42:21 -0600

    function3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -29,11 +29,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    var theProduct = a * b;
+    var message = 'The product of ' + a + ' and ' + b  + ' is ' + theProduct + '.';
+    return[theProduct, message]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var theSum = sum(a, sum(b,c)[0])[0];
+    var product = multiply(a, multiply(b,c)[0])[0];
+    var thirdElement = a + ' and ' + b + ' and ' + c + ' sum to ' + theSum + '.';
+    var fourthElement = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product + '.';
+    return [theSum, product, thirdElement, fourthElement];
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+//Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
