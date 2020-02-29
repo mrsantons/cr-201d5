e24621ea277cf4227ec5a4f2f882def96cf98644
Marie Antons <maantons@mail.fhsu.edu>
2020-02-29 09:20:53 -0600

    function5

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -99,12 +99,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+var arrayTest = [2, 3, 4];
+function multiplyArray([a, b, c]) { //eslint-disable-line
+var firstLine = multiply(a, b)[0];
+var secondLine = multiply(firstLine, c)[0];
+return [secondLine, "The numbers " + a + "," + b + "," + c + " have a product of " + secondLine + "."];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
